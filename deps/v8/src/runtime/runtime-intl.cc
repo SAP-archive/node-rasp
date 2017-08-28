3db7380874b7a76a876bef97405e98d81d986b5e
Patrick Spiegel <patrickspiegel@web.de>
2017-08-28 13:20:45 +0000

    [Taint] Propagation for String.prototpy.toUpperCase()

--- deps/v8/src/runtime/runtime-intl.cc
+++ deps/v8/src/runtime/runtime-intl.cc
@@ -636,7 +636,12 @@ RUNTIME_FUNCTION(Runtime_StringToUpperCaseIntl) {
   DCHECK_EQ(args.length(), 1);
   CONVERT_ARG_HANDLE_CHECKED(String, s, 0);
   s = String::Flatten(s);
-  return ConvertToUpper(s, isolate);
+  Object* result = ConvertToUpper(s, isolate);
+
+  // TaintV8
+  String::cast(result)->SetTaint(s->GetTaint());
+
+  return result;
 }
 
 RUNTIME_FUNCTION(Runtime_StringLocaleConvertCase) {
