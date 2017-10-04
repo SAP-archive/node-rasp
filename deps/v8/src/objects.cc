41e53181f51dc20df147445816454f65869b8514
Patrick Spiegel <patrick.spiegel@sap.com>
2017-10-04 12:26:34 +0000

    [Taint] Keep taint when ConsString is flattened

--- deps/v8/src/objects.cc
+++ deps/v8/src/objects.cc
@@ -2592,6 +2592,10 @@ Handle<String> String::SlowFlatten(Handle<ConsString> cons,
   }
   cons->set_first(*result);
   cons->set_second(isolate->heap()->empty_string());
+
+  // TaintV8
+  result->SetTaint(cons->GetTaint()); 
+  
   DCHECK(result->IsFlat());
   return result;
 }
