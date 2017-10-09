eab94d43545e56820765d944b99f4d8767c3bb11
Patrick Spiegel <patrickspiegel@web.de>
2017-10-09 16:00:16 +0000

    [Test] String indexes and implicitly the internal character table

---
+++ test/taint/test-string-index.js
@@ -0,0 +1,74 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const stringASCII_3 = 'foo';
+const stringUTF8_3 = '😃!';
+const stringASCII_300 = 'foo'.repeat(100);
+const stringUTF8_300 = '😃!'.repeat(100);
+
+const stringSet = [stringASCII_3, stringUTF8_3,
+                   stringASCII_300, stringUTF8_300];
+
+// assert for string taint
+assert.taintEqual = taintEqual;
+function taintEqual(string, expectedTaint) {
+  const actualTaint = string.getTaint();
+
+  assert.strictEqual(actualTaint.length, expectedTaint.length);
+
+  expectedTaint.forEach(function(range, i) {
+    assert.strictEqual(actualTaint[i].begin, range.begin);
+    assert.strictEqual(actualTaint[i].end, range.end);
+  });
+}
+
+stringSet.forEach((string) => {
+  const len = string.length;
+  let str = string.setTaint('bar'),
+    character;
+
+  assert.strictEqual(str.isTainted(), true);
+  assert.taintEqual(str, [{'begin': 0, 'end': len}]);
+
+  character = str[0];
+  assert.strictEqual(character.isTainted(), true);
+  assert.taintEqual(character, [{'begin': 0, 'end': 1}]);
+
+  character = str[1];
+  assert.strictEqual(character.isTainted(), true);
+  assert.taintEqual(character, [{'begin': 0, 'end': 1}]);
+
+  character = str[2];
+  assert.strictEqual(character.isTainted(), true);
+  assert.taintEqual(character, [{'begin': 0, 'end': 1}]);
+
+  str = str.removeTaint();
+  assert.strictEqual(str.isTainted(), false);
+  assert.taintEqual(str, []);
+
+  character = str[0];
+  assert.strictEqual(character.isTainted(), false);
+  assert.taintEqual(character, []);
+
+  character = str[1];
+  assert.strictEqual(character.isTainted(), false);
+  assert.taintEqual(character, []);
+
+  character = str[2];
+  assert.strictEqual(character.isTainted(), false);
+  assert.taintEqual(character, []);
+
+  str = str + str.setTaint('foo') + str;
+  character = str[2];
+  assert.strictEqual(character.isTainted(), false);
+  assert.taintEqual(character, []);
+
+  character = str[len];
+  assert.strictEqual(character.isTainted(), true);
+  assert.taintEqual(character, [{'begin': 0, 'end': 1}]);
+
+  character = str[len + len];
+  assert.strictEqual(character.isTainted(), false);
+  assert.taintEqual(character, []);
+});
