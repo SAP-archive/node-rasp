25607f2c3810b7a40439819f3de19c3ccab325b3
Patrick Spiegel <patrick.spiegel@sap.com>
2017-10-09 15:46:21 +0000

    [Test] Separate test files for string methods

---
+++ test/taint/test-string-basics.js
@@ -0,0 +1,53 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const stringASCII_3 = 'foo';
+const stringUTF8_3 = '😃!';
+const stringASCII_300 = 'foo'.repeat(100);
+const stringUTF8_300 = '😃!'.repeat(100);
+
+const stringSet = [stringASCII_3, stringUTF8_3,
+                   stringASCII_300, stringUTF8_300];
+
+// assert for string taint
+assert.taintEqual = taintEqual;
+function taintEqual(string, expectedTaint) {
+  const actualTaint = string.getTaint();
+
+  assert.strictEqual(actualTaint.length, expectedTaint.length);
+
+  expectedTaint.forEach(function(range, i) {
+    assert.strictEqual(actualTaint[i].begin, range.begin);
+    assert.strictEqual(actualTaint[i].end, range.end);
+  });
+}
+
+stringSet.forEach((string) => {
+  const len = string.length;
+  let str = string,
+    strTaint;
+
+  assert.strictEqual(str.isTainted(), false);
+  assert.taintEqual(str, []);
+
+  strTaint = str.setTaint('bar');
+  assert.strictEqual(str.isTainted(), false);
+  assert.taintEqual(str, []);
+  assert.strictEqual(strTaint.isTainted(), true);
+  assert.taintEqual(strTaint, [{'begin': 0, 'end': len}]);
+
+  str = str.removeTaint();
+  assert.strictEqual(str.isTainted(), false);
+  assert.taintEqual(str, []);
+
+  str = strTaint.removeTaint();
+  assert.strictEqual(str.isTainted(), false);
+  assert.taintEqual(str, []);
+  assert.strictEqual(strTaint.isTainted(), true);
+  assert.taintEqual(strTaint, [{'begin': 0, 'end': len}]);
+
+  strTaint = strTaint.removeTaint();
+  assert.strictEqual(strTaint.isTainted(), false);
+  assert.taintEqual(strTaint, []);
+});
