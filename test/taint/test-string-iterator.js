2a8509ae0ad1bca2873eae7d9bf98ca289a6ccb9
Patrick Spiegel <patrickspiegel@web.de>
2017-10-16 11:05:32 +0000

    [Test] String iterator with for of loop

---
+++ test/taint/test-string-iterator.js
@@ -0,0 +1,54 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const stringASCII_3 = 'foo';
+const stringUTF8_3 = '😃!';
+const stringASCII_300 = 'foo'.repeat(100);
+const stringUTF8_300 = '😃!'.repeat(100);
+
+const stringSet = [stringASCII_3, stringUTF8_3,
+                   stringASCII_300, stringUTF8_300];
+
+// assert for string taint
+assert.taintEqual = taintEqual;
+function taintEqual(string, expectedTaint) {
+  const actualTaint = string.getTaint();
+
+  assert.strictEqual(actualTaint.length, expectedTaint.length);
+
+  expectedTaint.forEach(function(range, i) {
+    assert.strictEqual(actualTaint[i].begin, range.begin);
+    assert.strictEqual(actualTaint[i].end, range.end);
+  });
+}
+
+stringSet.forEach((string) => {
+  let str = string.setTaint('bar');
+  const strIter = str[Symbol.iterator]();
+
+  for (let i = 0; i < str.length; i++) {
+    const v = strIter.next().value;
+    assert.strictEqual(v.isTainted(), true);
+    assert.taintEqual(v, [{'begin': 0, 'end': 1}]);
+  }
+
+  for (const v of str) {
+    assert.strictEqual(v.isTainted(), true);
+    assert.taintEqual(v, [{'begin': 0, 'end': 1}]);
+  }
+
+
+  str = string + str;
+  let i = 0;
+  for (const v of str) {
+    if (i < string.length) {
+      assert.strictEqual(v.isTainted(), false);
+      assert.taintEqual(v, []);
+    } else {
+      assert.strictEqual(v.isTainted(), true);
+      assert.taintEqual(v, [{'begin': 0, 'end': 1}]);
+    }
+    i++;
+  }
+});
