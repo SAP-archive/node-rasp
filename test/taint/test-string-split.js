cbaa417d0e36261b410091448163100df26cc160
Patrick Spiegel <patrickspiegel@web.de>
2017-10-17 12:49:29 +0000

    [Test] String.prototype.split()

---
+++ test/taint/test-string-split.js
@@ -0,0 +1,33 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+((string) => {
+  const len = string.length;
+  let str =
+    string.setTaint('bar') +
+    string.toLowerCase().setTaint('bar');
+  let parts;
+
+  parts = str.split('');
+  parts.forEach((part) => {
+    assert.strictEqual(part.isTainted(), true);
+    assert.taintEqual(part, [{'begin': 0, 'end': 1}]);
+  });
+
+  parts = str.split('XYZabc');
+  parts.forEach((part) => {
+    assert.strictEqual(part.isTainted(), true);
+    assert.taintEqual(part, [{'begin': 0, 'end': len - 3}]);
+  });
+
+  str = string + string.toLowerCase().setTaint('bar');
+  parts = str.split('XYZabc');
+  assert.strictEqual(parts[0].isTainted(), false);
+  assert.taintEqual(parts[0], []);
+  assert.strictEqual(parts[1].isTainted(), true);
+  assert.taintEqual(parts[1], [{'begin': 0, 'end': len - 3}]);
+
+})(alphabet);
