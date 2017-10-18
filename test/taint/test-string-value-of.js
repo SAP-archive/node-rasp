8f6fcf52d0580c071b0409b3b71ebd03ee565b6e
Patrick Spiegel <patrickspiegel@web.de>
2017-10-18 13:04:50 +0000

    [Test] String.prototype.valueOf()

---
+++ test/taint/test-string-value-of.js
@@ -0,0 +1,26 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+((string) => {
+  const str1 = 'abc123'.setTaint('bar');
+  const str2 = 'i\u0307'.setTaint('bar');
+  const str = str1 + 'bar' + str2;
+  let value;
+
+  value = str1.valueOf();
+  assert.strictEqual(value.isTainted(), true);
+  assert.taintEqual(value, [{'begin': 0, 'end': 6}]);
+
+  value = str2.valueOf();
+  assert.strictEqual(value.isTainted(), true);
+  assert.taintEqual(value, [{'begin': 0, 'end': 2}]);
+
+  value = str.valueOf();
+  assert.strictEqual(value.isTainted(), true);
+  assert.taintEqual(value, [
+    {'begin': 0, 'end': 6},
+    {'begin': 9, 'end': 11}
+  ]);
+
+})();
