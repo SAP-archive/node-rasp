c5994d7d0a458c9fe7292a37fd2b7c9a09e5f7da
Patrick Spiegel <patrickspiegel@web.de>
2017-10-18 11:01:50 +0000

    [Test] String.raw() with template strings

---
+++ test/taint/test-string-raw.js
@@ -0,0 +1,27 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+((string) => {
+  const name1 = 'Bob'.setTaint('bar');
+  const name2 = 'A' + 'lic'.setTaint('bar') + 'e';
+  let raw;
+
+  raw = String.raw`Hi ${name1}`;
+  assert.strictEqual(raw.isTainted(), true);
+  assert.taintEqual(raw, [{'begin': 3, 'end': 6}]);
+
+  raw = String.raw`Hey ${name2}!`;
+  assert.strictEqual(raw.isTainted(), true);
+  assert.taintEqual(raw, [{'begin': 5, 'end': 8}]);
+
+  raw = String.raw({ raw: name2 }, 0, 'baz'.setTaint('foo'), 2);
+  assert.strictEqual(raw.isTainted(), true);
+  assert.taintEqual(raw, [
+    {'begin': 2, 'end': 3},
+    {'begin': 3, 'end': 6},
+    {'begin': 6, 'end': 7},
+    {'begin': 8, 'end': 9}
+  ]);
+
+})();
