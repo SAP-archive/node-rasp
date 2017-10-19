f878da77be1b314b981959b3a51c8f3e41ca6ad7
Patrick Spiegel <patrickspiegel@web.de>
2017-10-19 13:27:21 +0000

    [Test] Global.decodeURIComponent()

---
+++ test/taint/test-global-decode-uri-component.js
@@ -0,0 +1,37 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const str1 = 'abc123';
+const str2 = '-_.!~*\'()';
+const str3 = '%2C%2F%3F%3A%40%26%3D%2B%24%23';
+const str4 = 'ABC%20abc%20123';
+
+(() => {
+  let decoded;
+
+  decoded = decodeURIComponent(str1.setTaint('foo'));
+  assert.strictEqual(decoded.isTainted(), true);
+  assert.taintEqual(decoded, [{'begin': 0, 'end': 6}]);
+
+  decoded = decodeURIComponent(str2.setTaint('foo'));
+  assert.strictEqual(decoded.isTainted(), true);
+  assert.taintEqual(decoded, [{'begin': 0, 'end': 9}]);
+
+  decoded = decodeURIComponent(str3.setTaint('foo'));
+  assert.strictEqual(decoded.isTainted(), true);
+  assert.taintEqual(decoded, [{'begin': 0, 'end': 10}]);
+
+  decoded = decodeURIComponent(str4.setTaint('foo'));
+  assert.strictEqual(decoded.isTainted(), true);
+  assert.taintEqual(decoded, [{'begin': 0, 'end': 11}]);
+
+  decoded = decodeURIComponent(
+    str2 + str4.setTaint('foo') + str3 + str4.setTaint('baz'));
+  assert.strictEqual(decoded.isTainted(), true);
+  assert.taintEqual(decoded, [
+    {'begin': 9, 'end': 20},
+    {'begin': 30, 'end': 41}
+  ]);
+
+})();
