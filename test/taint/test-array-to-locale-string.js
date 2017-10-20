4d0f5de49e90ce24de245ed2237743df588e856e
Patrick Spiegel <patrickspiegel@web.de>
2017-10-20 12:13:39 +0000

    [Test] Array.prototype.toLocaleString()

---
+++ test/taint/test-array-to-locale-string.js
@@ -0,0 +1,40 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const array1 = ['i\u0307'.setTaint('bar')];
+const array2 = [42, 'foo'.setTaint('bar')];
+const array3 = ['foo'.setTaint('bar'), 'baz'.setTaint('foo')];
+const array4 = ['foo'.setTaint('bar'), 'some', 'baz'.setTaint('foo')];
+const array5 = ['f' + 'o'.setTaint('bar') + 'o', 'some'];
+
+(() => {
+  let string;
+
+  string = array1.toLocaleString('lt-LT');
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [{'begin': 0, 'end': 2}]);
+
+  string = array2.toLocaleString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [{'begin': 3, 'end': 6}]);
+
+  string = array3.toLocaleString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [
+    {'begin': 0, 'end': 3},
+    {'begin': 4, 'end': 7}
+  ]);
+
+  string = array4.toLocaleString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [
+    {'begin': 0, 'end': 3},
+    {'begin': 9, 'end': 12}
+  ]);
+
+  string = array5.toLocaleString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [{'begin': 1, 'end': 2}]);
+
+})();
