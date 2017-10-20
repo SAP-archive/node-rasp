b304e1f6fea9fb2eb10f51b2e08be503a83cfa52
Patrick Spiegel <patrickspiegel@web.de>
2017-10-20 11:35:29 +0000

    [Test] Array.prototype.toString()

---
+++ test/taint/test-array-to-string.js
@@ -0,0 +1,40 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const array1 = ['foo'.setTaint('bar')];
+const array2 = [42, 'foo'.setTaint('bar')];
+const array3 = ['foo'.setTaint('bar'), 'baz'.setTaint('foo')];
+const array4 = ['foo'.setTaint('bar'), 'some', 'baz'.setTaint('foo')];
+const array5 = ['f' + 'o'.setTaint('bar') + 'o', 'some'];
+
+(() => {
+  let string;
+
+  string = array1.toString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [{'begin': 0, 'end': 3}]);
+
+  string = array2.toString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [{'begin': 3, 'end': 6}]);
+
+  string = array3.toString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [
+    {'begin': 0, 'end': 3},
+    {'begin': 4, 'end': 7}
+  ]);
+
+  string = array4.toString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [
+    {'begin': 0, 'end': 3},
+    {'begin': 9, 'end': 12}
+  ]);
+
+  string = array5.toString();
+  assert.strictEqual(string.isTainted(), true);
+  assert.taintEqual(string, [{'begin': 1, 'end': 2}]);
+
+})();
