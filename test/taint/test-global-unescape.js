d53b83a1ae913b25c074033863585ccb2c76f9ea
Patrick Spiegel <patrickspiegel@web.de>
2017-10-20 08:11:09 +0000

    [Test] Global.unescape()

---
+++ test/taint/test-global-unescape.js
@@ -0,0 +1,37 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const str1 = 'abc123';
+const str2 = '%E4%F6%FC';
+const str3 = '%u0107';
+const str4 = '%3Cscript%3E';
+
+(() => {
+  let unescaped;
+
+  unescaped = unescape(str1.setTaint('foo'));
+  assert.strictEqual(unescaped.isTainted(), true);
+  assert.taintEqual(unescaped, [{'begin': 0, 'end': 6}]);
+
+  unescaped = unescape(str2.setTaint('foo'));
+  assert.strictEqual(unescaped.isTainted(), true);
+  assert.taintEqual(unescaped, [{'begin': 0, 'end': 3}]);
+
+  unescaped = unescape(str3.setTaint('foo'));
+  assert.strictEqual(unescaped.isTainted(), true);
+  assert.taintEqual(unescaped, [{'begin': 0, 'end': 1}]);
+
+  unescaped = unescape(str4.setTaint('foo'));
+  assert.strictEqual(unescaped.isTainted(), true);
+  assert.taintEqual(unescaped, [{'begin': 0, 'end': 8}]);
+
+  unescaped = unescape(
+    str2 + str4.setTaint('foo') + str3 + str4.setTaint('baz'));
+  assert.strictEqual(unescaped.isTainted(), true);
+  assert.taintEqual(unescaped, [
+    {'begin': 3, 'end': 11},
+    {'begin': 12, 'end': 20}
+  ]);
+
+})();
