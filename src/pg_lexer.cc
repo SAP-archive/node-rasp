1979af3fc92ead1ed750d7ceb927f2137deb45a5
Patrick Spiegel <patrickspiegel@web.de>
2018-04-17 08:58:57 +0000

    [Lint] Use nullptr instead of NULL

--- src/pg_lexer.cc
+++ src/pg_lexer.cc
@@ -31,7 +31,7 @@ NODE_EXTERN Local<Array> HasInjection(Environment* env, const char* data) {
   if (result.error) {
     ranges = Array::New(env->isolate(), 0);
   } else {
-    scan_output = pgquery__scan_output__unpack(NULL, result.pbuf_len,
+    scan_output = pgquery__scan_output__unpack(nullptr, result.pbuf_len,
                                   static_cast<uint8_t *>(result.pbuf));
 
     ranges = Array::New(env->isolate(), scan_output->n_tokens);
@@ -51,7 +51,7 @@ NODE_EXTERN Local<Array> HasInjection(Environment* env, const char* data) {
 
       ranges->Set(j, range);
     }
-    pgquery__scan_output__free_unpacked(scan_output, NULL);
+    pgquery__scan_output__free_unpacked(scan_output, nullptr);
   }
   pg_query_free_scan_result(result);
   return ranges;
