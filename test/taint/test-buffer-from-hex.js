589b112fcbac230cfb77c20b7bc462c396b5e279
Tobias Simolik <tobias.simolik@sap.com>
2018-08-01 09:58:40 +0200

    [Buffer] Taint API harmonization (#56)
    * [Refactoring] change taint to _taint on buffer
    
    * [Refactor] Added _taint in unit tests
    
    * [Feature] Added method for buffer
    
    * [Test] Buffer.new() test for each encoding
    
    * [Test] Buffer.from() tests
    
    * [Test] Buffer.new() tests
    
    * [Test] Buffer.new() tests for one string + each encoding
    
    * [Test] Buffer.new() tests for concatenated ascii and utf8 encoding
    
    * [Test] Buffer.new() test enhanced for utf8 encoding
    
    * [Test] Buffer.new() tests for hex (ascii) encoding
    
    * [Test] Buffer.new() removed bug in unit tests
    
    * [Test] Buffer.new() tests for hex encoding with utf8 character
    
    * [Test] Buffer.new() unit tests for base64 encoding
    
    * [Test] Buffer.new() unit tests for base64 unicode encoding
    
    * [Test] Buffer.new() refactoring for utf8 + ascii encoding
    
    * [Buffer] Reactor taint propagation toString and slice
    
    * [Buffer] Remove previously inserted newlines
    
    * [Buffer] Taint propagation for Buffer.prototype.toString('hex')
    
    * [Test] Buffer.new() unit tests for utf8 encoding
    
    * [Feature] Buffer.alloc() keep taint
    
    * [Feature] Buffer.alloc() keep (sub-)taint for hex encoding
    
    * [Test] Buffer.fill() unit tests
    
    * [Feature] Buffer.write() ascii/uft8 taint propagation
    
    * [Test] Buffer.concat() unit tests
    
    * Buffer.write() refactored
    
    * Â[Feature] apply Taint to Partial Buffer
    
    * [Feature] Keep taint outsite taint ranges for write/fill
    
    * [Fix] Buffer.concat() taint propagation
    
    * [Merge] buffer refactoring
    
    * [Refactoring] Final buffer status
    
    * [Feature] Final status of buffer
    
    * [Feature] Final refactored buffer api status

---
+++ test/taint/test-buffer-from-hex.js
@@ -0,0 +1,182 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+// ASCII
+const strAscii = 'This string is not tainted!';
+const strAsciiTaint = 'This is a tainted string!'.setTaint('baz');
+
+const strAsciiLen = strAscii.length;
+const strAsciiTaintLen = strAsciiTaint.length;
+
+// HEX
+const strHex = Buffer.from(strAscii, 'ascii').toString('hex');
+const strHexTaint = Buffer.from(strAsciiTaint, 'ascii').toString('hex')
+                                                       .setTaint('abc');
+const strHexLen = Buffer.from(strAscii, 'ascii').toString('hex').length;
+const strHexTaintLen = Buffer.from(strAsciiTaint, 'ascii')
+                                                .toString('hex').length;
+const bufHexLen = Buffer.from(strHex, 'hex').length;
+const bufHexTaintLen = Buffer.from(strHexTaint, 'hex').length;
+
+let resultString;
+let taintEnd;
+let taintEnd2;
+let taintStart;
+let taintStart2;
+
+// ### ONE STRING ###
+
+// Hex string in buffer is tainted.
+const buf01 = Buffer.from(strHexTaint, 'hex');
+assert.taintEqual(buf01, [{ 'begin': 0, 'end': bufHexTaintLen }]);
+// Test HEX
+resultString = buf01.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': 0, 'end': strHexTaintLen }]);
+// Test ASCII
+resultString = buf01.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': 0, 'end': strAsciiTaintLen }]);
+
+// Hex string in buffer is not tainted.
+const buf02 = Buffer.from(strHex, 'hex');
+assert.taintEqual(buf02, []);
+// Test HEX
+resultString = buf02.toString('hex');
+assert.strictEqual(resultString.isTainted(), false);
+assert.taintEqual(resultString, []);
+// Test ASCII
+resultString = buf02.toString('ascii');
+assert.strictEqual(resultString.isTainted(), false);
+assert.taintEqual(resultString, []);
+
+// ### Concatenation of strings ###
+
+// Concatenated hex string: One tainted string
+const buf03 = Buffer.from(strHex + strHexTaint + strHex, 'hex');
+taintStart = bufHexLen;
+taintEnd = taintStart + bufHexTaintLen;
+assert.taintEqual(buf03, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test HEX
+resultString = buf03.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strHexLen;
+taintEnd = taintStart + strHexTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test ASCII
+resultString = buf03.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Concatenated hex string: Two same tainted string
+const buf04 = Buffer.from(strHex + strHexTaint + strHexTaint +
+                                                strHex, 'hex');
+taintStart = bufHexLen;
+taintEnd = taintStart + bufHexTaintLen * 2;
+assert.taintEqual(buf04, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test HEX
+resultString = buf04.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strHexLen;
+taintEnd = taintStart + strHexTaintLen * 2;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test ASCII
+resultString = buf04.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen * 2;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Concatenated hex string: Two different tainted string
+const buf05 = Buffer.from(strHex + strHexTaint.setTaint('different') +
+                                  strHexTaint + strHex, 'hex');
+taintStart = bufHexLen;
+taintEnd = taintStart + bufHexTaintLen;
+taintStart2 = taintEnd;
+taintEnd2 = taintStart2 + bufHexTaintLen;
+assert.taintEqual(buf05, [{ 'begin': taintStart, 'end': taintEnd },
+                          { 'begin': taintStart2, 'end': taintEnd2 }]);
+// Test HEX
+resultString = buf05.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strHexLen;
+taintEnd = taintStart + strHexTaintLen;
+taintStart2 = taintEnd;
+taintEnd2 = taintStart2 + strHexTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
+// Test ASCII
+resultString = buf05.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen;
+taintStart2 = taintEnd;
+taintEnd2 = taintStart2 + strAsciiTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
+
+// HEX string: Taint at the beginning
+const buf06 = Buffer.from(strHexTaint + strHex, 'hex');
+taintStart = 0;
+taintEnd = bufHexTaintLen;
+assert.taintEqual(buf06, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test HEX
+resultString = buf06.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = 0;
+taintEnd = strHexTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test ASCII
+resultString = buf06.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = 0;
+taintEnd = strAsciiTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// HEX string: Taint at the end
+const buf07 = Buffer.from(strHex + strHexTaint, 'hex');
+taintStart = bufHexLen;
+taintEnd = taintStart + bufHexTaintLen;
+assert.taintEqual(buf07, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test HEX
+resultString = buf07.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strHexLen;
+taintEnd = taintStart + strHexTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+// Test ASCII
+resultString = buf07.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Hex String at the beginning and the end
+const buf08 = Buffer.from(strHexTaint + strHex + strHexTaint, 'hex');
+taintStart = 0;
+taintEnd = bufHexTaintLen;
+taintStart2 = taintEnd + bufHexLen;
+taintEnd2 = taintStart2 + bufHexTaintLen;
+assert.taintEqual(buf08, [{ 'begin': taintStart, 'end': taintEnd },
+                          { 'begin': taintStart2, 'end': taintEnd2 }]);
+// Test HEX
+resultString = buf08.toString('hex');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = 0;
+taintEnd = strHexTaintLen;
+taintStart2 = taintEnd + strHexLen;
+taintEnd2 = taintStart2 + strHexTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
+// Test ASCII
+resultString = buf08.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+taintStart = 0;
+taintEnd = strAsciiTaintLen;
+taintStart2 = taintEnd + strAsciiLen;
+taintEnd2 = taintStart2 + strAsciiTaintLen;
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
---
+++ test/taint/test-buffer-new-ascii.js
@@ -0,0 +1,122 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+// ASCII
+const strAscii = 'This string is not tainted!';
+const strAsciiTaint = 'This is a tainted string!'.setTaint('baz');
+
+const strAsciiLen = strAscii.length;
+const strAsciiTaintLen = strAsciiTaint.length;
+
+const bufAsciiTaintLen = Buffer.from(strAsciiTaint).length;
+
+let resultString;
+let taintEnd;
+let taintEnd2;
+let taintStart;
+let taintStart2;
+
+// ### ONE STRING ###
+
+// Ascii string in buffer is tainted.
+const buf01 = new Buffer(strAsciiTaint, 'ascii');
+assert.taintEqual(buf01, [{ 'begin': 0, 'end': bufAsciiTaintLen }]);
+resultString = buf01.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': 0, 'end': strAsciiTaintLen }]);
+
+// Ascii string in buffer is not tainted.
+const buf02 = new Buffer(strAscii, 'ascii');
+assert.taintEqual(buf02, []);
+resultString = buf02.toString('ascii');
+assert.strictEqual(resultString.isTainted(), false);
+assert.taintEqual(resultString, []);
+
+// ### Concatenation of strings ###
+
+// One tainted string
+const buf03 = new Buffer(strAscii + strAsciiTaint + strAscii, 'ascii');
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen;
+assert.taintEqual(buf03, [{ 'begin': taintStart, 'end': taintEnd }]);
+resultString = buf03.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Two same tainted strings
+const buf04 = new Buffer(strAscii + strAsciiTaint + strAsciiTaint +
+                                                    strAscii, 'ascii');
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen + strAsciiTaintLen;
+assert.taintEqual(buf04, [{ 'begin': taintStart, 'end': taintEnd }]);
+resultString = buf04.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Two different tainted strings
+const buf05 = new Buffer(strAscii + strAsciiTaint.setTaint('different') +
+                                    strAsciiTaint + strAscii, 'ascii');
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen;
+taintStart2 = taintEnd;
+taintEnd2 = taintStart2 + strAsciiTaintLen;
+assert.taintEqual(buf05, [{ 'begin': taintStart, 'end': taintEnd },
+                          { 'begin': taintStart2, 'end': taintEnd2 }]);
+resultString = buf05.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
+
+// Taint at the beginning
+const buf06 = new Buffer(strAsciiTaint + strAscii, 'ascii');
+taintStart = 0;
+taintEnd = strAsciiTaintLen;
+assert.taintEqual(buf06, [{ 'begin': taintStart, 'end': taintEnd }]);
+resultString = buf06.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Taint at the end
+const buf07 = new Buffer(strAscii + strAsciiTaint, 'ascii');
+taintStart = strAsciiLen;
+taintEnd = taintStart + strAsciiTaintLen;
+assert.taintEqual(buf07, [{ 'begin': taintStart, 'end': taintEnd }]);
+resultString = buf07.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd }]);
+
+// Taint at the beginning and the end
+const buf08 = new Buffer(strAsciiTaint + strAscii + strAsciiTaint, 'ascii');
+taintStart = 0;
+taintEnd = strAsciiTaintLen;
+taintStart2 = taintEnd + strAsciiLen;
+taintEnd2 = taintStart2 + strAsciiTaintLen;
+assert.taintEqual(buf08, [{ 'begin': taintStart, 'end': taintEnd },
+                          { 'begin': taintStart2, 'end': taintEnd2 }]);
+resultString = buf08.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
+
+// ### COPY BUFFER ###
+
+// Tainted buffer
+const buf09 = new Buffer(buf01);
+assert.taintEqual(buf09, [{ 'begin': 0, 'end': strAsciiTaintLen }]);
+resultString = buf09.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': 0, 'end': strAsciiTaintLen }]);
+
+// Multiple taints
+const buf10 = new Buffer(buf08);
+taintStart = 0;
+taintEnd = strAsciiTaintLen;
+taintStart2 = taintEnd + strAsciiLen;
+taintEnd2 = taintStart2 + strAsciiTaintLen;
+assert.taintEqual(buf10, [{ 'begin': taintStart, 'end': taintEnd },
+                          { 'begin': taintStart2, 'end': taintEnd2 }]);
+resultString = buf10.toString('ascii');
+assert.strictEqual(resultString.isTainted(), true);
+assert.taintEqual(resultString, [{ 'begin': taintStart, 'end': taintEnd },
+                                 { 'begin': taintStart2, 'end': taintEnd2 }]);
