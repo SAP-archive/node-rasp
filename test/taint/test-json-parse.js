7e3aae25753c283d8e319428dda3a7ef1147854a
Patrick Spiegel <patrickspiegel@web.de>
2019-02-03 18:18:47 +0000

    [Test] JSON.parse

---
+++ test/taint/test-json-parse.js
@@ -0,0 +1,26 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+((string) => {
+  const str1 = '{"foo":"bar"}'.taint('baz');
+  const str2 = '{"f' + 'oo":"b'.taint('baz') + 'ar"}';
+  let parsed, key, value;
+
+  parsed = JSON.parse(str1);
+  key = Object.keys(parsed)[0];
+  value = parsed.foo;
+  assert.strictEqual(key.isTainted(), true);
+  assert.taintEqual(key, [{ 'begin': 0, 'end': 3 }]);
+  assert.strictEqual(value.isTainted(), true);
+  assert.taintEqual(value, [{ 'begin': 0, 'end': 3 }]);
+
+  parsed = JSON.parse(str2);
+  key = Object.keys(parsed)[0];
+  value = parsed.foo;
+  assert.strictEqual(key.isTainted(), true);
+  assert.taintEqual(key, [{ 'begin': 1, 'end': 3 }]);
+  assert.strictEqual(value.isTainted(), true);
+  assert.taintEqual(value, [{ 'begin': 0, 'end': 1 }]);
+
+})();
