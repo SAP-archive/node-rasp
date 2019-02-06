372b6561542b2e9595ee91b25b2eac956fa01ebf
Patrick Spiegel <patrickspiegel@web.de>
2019-02-06 12:13:28 +0000

    [Test] String.prototype.split

---
+++ test/taint/test-string-split.js
@@ -0,0 +1,30 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const len = string.length;
+let str =
+  string.taint('bar') +
+  string.toLowerCase().taint('bar');
+let parts;
+
+parts = str.split('');
+parts.forEach((part) => {
+  assert.strictEqual(part.isTainted(), true);
+  assert.taintEqual(part, [{ 'begin': 0, 'end': 1 }]);
+});
+
+parts = str.split('XYZabc');
+parts.forEach((part) => {
+  assert.strictEqual(part.isTainted(), true);
+  assert.taintEqual(part, [{ 'begin': 0, 'end': len - 3 }]);
+});
+
+str = string + string.toLowerCase().taint('bar');
+parts = str.split('XYZabc');
+assert.strictEqual(parts[0].isTainted(), false);
+assert.taintEqual(parts[0], []);
+assert.strictEqual(parts[1].isTainted(), true);
+assert.taintEqual(parts[1], [{ 'begin': 0, 'end': len - 3 }]);
