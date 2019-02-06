5c8f2847b52468f05c0c7dfb67a4e758e8db1bd0
Patrick Spiegel <patrickspiegel@web.de>
2019-02-06 12:03:07 +0000

    [Test] String.prototype.match

---
+++ test/taint/test-string-match.js
@@ -0,0 +1,29 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+
+const string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let str =
+  string.taint('bar') +
+  string.toLowerCase().taint('bar');
+let matches;
+
+matches = str.match(/[A-E]/gi);
+matches.forEach((match) => {
+  assert.strictEqual(match.isTainted(), true);
+  assert.taintEqual(match, [{ 'begin': 0, 'end': 1 }]);
+});
+
+matches = str.match('abc');
+matches.forEach((match) => {
+  assert.strictEqual(match.isTainted(), true);
+  assert.taintEqual(match, [{ 'begin': 0, 'end': 3 }]);
+});
+
+str = string + string.toLowerCase().taint('bar');
+matches = str.match(/XYZabc/g);
+matches.forEach((match) => {
+  assert.strictEqual(match.isTainted(), true);
+  assert.taintEqual(match, [{ 'begin': 3, 'end': 6 }]);
+});
