8e8f4af1c184e88d2d66eb3e708f757f8052dfcf
Patrick Spiegel <patrickspiegel@web.de>
2019-02-09 23:57:52 +0000

    [Test] Data privacy of logs

---
+++ test/taint/test-data-privacy.js
@@ -0,0 +1,28 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+const _pg = require('taint/_pg');
+
+// Wrap and assert console.info
+console.info = (...args) => {
+  args.forEach((arg) => {
+    assert.strictEqual(arg.isTainted(), false);
+  });
+};
+
+// Instrument pseudo pg module
+const pg = {};
+pg.Client = {};
+pg.Client.prototype = {};
+pg.Client.prototype.query = () => {};
+_pg.instrument(pg);
+
+pg.Client.prototype.query('SELECT foo FROM bar'.taint('bar'));
+pg.Client.prototype.query('SELECT foo FROM bar WHERE b=\'a' +
+                          'bc\' OR 1=1;--'.taint('bar') +
+                          '\';');
+pg.Client.prototype.query('SELECT foo FROM bar WHERE b=\'a' +
+                          'bc\' OR 1=1;--'.taint('bar') +
+                          '\' OR baz=\'' +
+                          '\' OR tenant>0;--'.taint('baz') +
+                          '\';');
